1cc91111bca2c7135548685c0e3532c163d8582f
unknown <phil.j.p.frey@gmail.com>
2019-10-19 20:02:04 -0400

    Made Client reset when message sent Errors may occur on linux, another branch will be made for it

--- ClientBase.py
+++ ClientBase.py
@@ -1,10 +1,13 @@
 import socket               # Import socket module
+import os
 
-s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12364                # Reserve a port for your service.
-s.connect((host, port))
-print s.recv(2048)
-packet = raw_input("Message");
-s.send(packet)
-s.close()                     # Close the socket when done
+while True:
+    s = socket.socket()         # Create a socket object
+    s.connect((host, port))
+    print s.recv(2048)
+    packet = raw_input("Message");
+    s.send(packet)
+    s.close()                     # Close the socket when done
+    os.system("cls");
